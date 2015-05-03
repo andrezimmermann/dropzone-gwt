8fcf2d2b83425e3c78a3bc94d30b7c0dc1b69696
André Zimmermann <andrezimmermann@email.com>
2015-05-03 13:20:43 -0300

    Added few comments

--- src/main/java/com/twocentsforthoughts/dropzone/client/DropzoneEventHandlerAdaptor.java
+++ src/main/java/com/twocentsforthoughts/dropzone/client/DropzoneEventHandlerAdaptor.java
@@ -14,51 +14,62 @@ public class DropzoneEventHandlerAdaptor implements DropzoneEventHandler {
 
   @Override
   public void onAddedFile(File file) {
+    // NOOP
   }
 
   @Override
   public void onCancelled(File file) {
+    // NOOP
   }
 
   @Override
   public void onComplete(File file) {
+    // NOOP
   }
 
   @Override
   public void onError(File file, String message, XHRObjet xhrObject) {
+    // NOOP
   }
 
   @Override
   public void onMaxFilesExceeded(File file) {
+    // NOOP
   }
 
   @Override
   public void onMaxFilesReached(File file) {
+    // NOOP
   }
 
   @Override
   public void onProcessing(File file) {
+    // NOOP
   }
 
   @Override
   public void onRemovedfile(File file) {
+    // NOOP
   }
 
   @Override
   public void onSending(File file, FormData formData, XHRObjet xhrObject) {
+    // NOOP
   }
 
   @Override
   public void onSuccess(File file, String response) {
-
+    // NOOP
   }
 
   @Override
   public void onThumbnail(File file, String dataURI) {
+    // NOOP
   }
 
   @Override
   public void onUploadProgress(File file, int progressSent, int bytesSent) {
+    // NOOP
   }
 
 }
