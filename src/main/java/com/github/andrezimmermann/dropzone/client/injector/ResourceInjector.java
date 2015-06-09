12bed8727fc94684900548d58d9c41dfe45a9d5e
andrezimmermann <sirreto@gmail.com>
2015-06-09 08:34:14 -0300

    No need to escape anything, GSS used was propertly converted

--- src/main/java/com/github/andrezimmermann/dropzone/client/injector/ResourceInjector.java
+++ src/main/java/com/github/andrezimmermann/dropzone/client/injector/ResourceInjector.java
@@ -5,43 +5,32 @@ import com.google.gwt.dom.client.StyleInjector;
 import com.google.gwt.resources.client.CssResource;
 import com.google.gwt.resources.client.TextResource;
 
+/**
+ * Responsible for injecting the resources within the application
+ * @author andrezimmermmann
+ *
+ */
 public class ResourceInjector {
 
-  private static boolean configured;
+    private static boolean configured;
 
-  private ResourceInjector() {
-  }
-
-  public static void configure(Resources resources) {
-    if (!configured) {
-      injectCss(resources.dropzoneCss());
-      injectJs(resources.dropzoneJs());
-      configured = true;
+    public static void configure(Resources resources) {
+        if (!configured) {
+            injectCss(resources.dropzoneCss());
+            injectJs(resources.dropzoneJs());
+            configured = true;
+        }
     }
-  }
 
-  /**
-   * <p>
-   * This injector allows using CSS3 styles in resources, at least to some degree.
-   * GWT does not parse CSS3 styles, however, it is possible to use them by escaping
-   * functionality that can not be parsed.
-   * </p>
-   * <p>
-   * E.g. {@code .dz-filename:not(:hover )} should be written as {@code .dz-filename:not\(:hover \)}
-   * Resulting css will contain an extra backslash: {@code .dz-filename:not(:hover \)} which causes
-   * problems in newer browser. So, code below removes all occurrences of " \)" and provides regular css.
-   * </p>
-   *
-   * @param r css resource to inject
-   */
-  private static void injectCss(CssResource r) {
-    String css = r.getText();
-    css = css.replaceAll(" \\\\\\)", ")");
-    StyleInjector.inject(css, true);
-  }
+    private static void injectCss(CssResource resource) {
+        StyleInjector.injectAtStart(resource.getText(), true);
+    }
 
-  private static void injectJs(TextResource r) {
-    JavaScriptInjector.inject(r.getText());
-  }
+    private static void injectJs(TextResource r) {
+        JavaScriptInjector.inject(r.getText());
+    }
+
+    private ResourceInjector() {
+    }
 
 }
