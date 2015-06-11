92c12d3dc7355b78d37edd7b855f72573429ba06
andrezimmermann <andrezimmermann@operamail.com>
2015-06-10 23:33:13 -0300

    Added removeFile and added tests fix #16

--- src/main/java/com/github/andrezimmermann/dropzone/client/Dropzone.java
+++ src/main/java/com/github/andrezimmermann/dropzone/client/Dropzone.java
@@ -23,77 +23,77 @@ import com.google.gwt.user.client.ui.HTML;
  */
 public class Dropzone extends Composite {
 
-    /**
-     * Create the object that contains the Dictionary used by {@link Dropzone}
-     *
-     * @return a default (en-us) {@link DropzoneDictonary} instance
-     */
-    public static DropzoneDictonary dictionary() {
-        return Dictionary.create();
-    }
+	/**
+	 * Create the object that contains the Dictionary used by {@link Dropzone}
+	 *
+	 * @return a default (en-us) {@link DropzoneDictonary} instance
+	 */
+	public static DropzoneDictonary dictionary() {
+		return Dictionary.create();
+	}
 
-    /**
-     * Create the object that contains the Options used by {@link Dropzone}
-     *
-     * @return a default {@link DropzoneOptions} instance
-     */
-    public static DropzoneOptions options() {
-        return Options.create();
-    }
+	/**
+	 * Create the object that contains the Options used by {@link Dropzone}
+	 *
+	 * @return a default {@link DropzoneOptions} instance
+	 */
+	public static DropzoneOptions options() {
+		return Options.create();
+	}
 
-    private final DropzoneOptions options;
+	private final DropzoneOptions options;
 
-    private final DropzoneEventHandler handler;
+	private final DropzoneEventHandler handler;
 
-    private final DropzoneDictonary dictionary;
+	private final DropzoneDictonary dictionary;
 
-    public Dropzone(DropzoneOptions options) {
-        this(options, null, null, (Resources) GWT.create(Resources.class));
-    }
+	public Dropzone(DropzoneOptions options) {
+		this(options, null, null, (Resources) GWT.create(Resources.class));
+	}
 
-    public Dropzone(DropzoneOptions options, DropzoneDictonary dictionary) {
-        this(options, null, dictionary, (Resources) GWT.create(Resources.class));
-    }
+	public Dropzone(DropzoneOptions options, DropzoneDictonary dictionary) {
+		this(options, null, dictionary, (Resources) GWT.create(Resources.class));
+	}
 
-    public Dropzone(DropzoneOptions options, DropzoneEventHandler handler) {
-        this(options, handler, null, (Resources) GWT.create(Resources.class));
-    }
+	public Dropzone(DropzoneOptions options, DropzoneEventHandler handler) {
+		this(options, handler, null, (Resources) GWT.create(Resources.class));
+	}
 
-    public Dropzone(DropzoneOptions options, DropzoneEventHandler handler, DropzoneDictonary dictionary) {
-        this(options, handler, dictionary, (Resources) GWT.create(Resources.class));
-    }
+	public Dropzone(DropzoneOptions options, DropzoneEventHandler handler, DropzoneDictonary dictionary) {
+		this(options, handler, dictionary, (Resources) GWT.create(Resources.class));
+	}
 
-    public Dropzone(DropzoneOptions options, DropzoneEventHandler handler, DropzoneDictonary dictionary,
-        Resources resources) {
-        this.options = options;
-        this.handler = handler;
-        this.dictionary = dictionary;
-        injectResources(resources);
-        initWidget();
-        initDropzone(getElement(), options, handler, dictionary);
-    }
+	public Dropzone(DropzoneOptions options, DropzoneEventHandler handler, DropzoneDictonary dictionary,
+			Resources resources) {
+		this.options = options;
+		this.handler = handler;
+		this.dictionary = dictionary;
+		injectResources(resources);
+		initWidget();
+		initDropzone(getElement(), options, handler, dictionary);
+	}
 
-    /**
-     * Add file information manually to Dropzone. Added file is not uploaded, it is just shown in the dropzone.
-     * This feature is useful for displaying e.g. files that already exists on the server.
-     *
-     * @param fileName     name of file to add
-     * @param fileSize     size of file to add
-     * @param thumbnailUrl thumbnail image for file
-     */
-    public void addFile(String fileName, Integer fileSize, String thumbnailUrl) {
-        addFileNative(getElement(), fileName, fileSize, thumbnailUrl);
-    }
+	/**
+	 * Add file information manually to Dropzone. Added file is not uploaded, it is just shown in the dropzone.
+	 * This feature is useful for displaying e.g. files that already exists on the server.
+	 *
+	 * @param fileName     name of file to add
+	 * @param fileSize     size of file to add
+	 * @param thumbnailUrl thumbnail image for file
+	 */
+	public void addFile(String fileName, Integer fileSize, String thumbnailUrl) {
+		addFileNative(getElement(), fileName, fileSize, thumbnailUrl);
+	}
 
-    /**
-     * Native implementation of {@link Dropzone#addFile(String, Integer, String)} method.
-     *
-     * @param e            dropzone element
-     * @param fileName     name of file to add
-     * @param fileSize     size of file to add
-     * @param thumbnailUrl thumbnail image for file
-     */
-    private native void addFileNative(Element e, String fileName, Integer fileSize, String thumbnailUrl) /*-{
+	/**
+	 * Native implementation of {@link Dropzone#addFile(String, Integer, String)} method.
+	 *
+	 * @param e            dropzone element
+	 * @param fileName     name of file to add
+	 * @param fileSize     size of file to add
+	 * @param thumbnailUrl thumbnail image for file
+	 */
+	private native void addFileNative(Element e, String fileName, Integer fileSize, String thumbnailUrl) /*-{
 		var mockFile = {
 			name : fileName,
 			size : fileSize,
@@ -107,57 +107,59 @@ public class Dropzone extends Composite {
 		e.dropzone.emit("success", mockFile);
     }-*/;
 
-    /**
-     * Return information about particular file added to dropzone.
-     *
-     * @param index of the file
-     * @return file information
-     */
-    public File getFile(int index) {
-        return getFilesNative(getElement()).get(index);
-    }
+	/**
+	 * Return information about particular file added to dropzone.
+	 *
+	 * @param index of the file
+	 * @return file information
+	 */
+	public File getFile(int index) {
+		return getFilesNative(getElement()).get(index);
+	}
 
-    /**
-     * Return array if all files added to dropzone.
-     *
-     * @return all files added to dropzone.
-     */
-    public ArrayList<File> getFiles() {
-        final JsArray<FileJS> filesJS = getFilesNative(getElement());
-        final ArrayList<File> files = new ArrayList<File>();
+	/**
+	 * Return array if all files added to dropzone.
+	 *
+	 * @return all files added to dropzone.
+	 */
+	public ArrayList<File> getFiles() {
+		//TODO Why ArrayList, change to list when in a proper IDE
 
-        if (filesJS != null) {
-            for (int i = 0; i < filesJS.length(); i++) {
-                files.add(filesJS.get(i));
-            }
-        }
+		final JsArray<FileJS> filesJS = getFilesNative(getElement());
+		final ArrayList<File> files = new ArrayList<File>();
 
-        return files;
-    }
+		if (filesJS != null) {
+			for (int i = 0; i < filesJS.length(); i++) {
+				files.add(filesJS.get(i));
+			}
+		}
 
-    /**
-     * Return number of added files.
-     *
-     * @return number of files
-     */
-    public int getFilesCount() {
-        final JsArray<FileJS> files = getFilesNative(getElement());
-        return files != null ? files.length() : 0;
-    }
+		return files;
+	}
 
-    /**
-     * Native implementation of {@Link Dropzone#getFiles()}
-     *
-     * @param e dropzone element
-     * @return array of {@link FileJS} elements
-     */
-    private native JsArray<FileJS> getFilesNative(Element e) /*-{
+	/**
+	 * Return number of added files.
+	 *
+	 * @return number of files
+	 */
+	public int getFilesCount() {
+		final JsArray<FileJS> files = getFilesNative(getElement());
+		return files != null ? files.length() : 0;
+	}
+
+	/**
+	 * Native implementation of {@Link Dropzone#getFiles()}
+	 *
+	 * @param e dropzone element
+	 * @return array of {@link FileJS} elements
+	 */
+	private native JsArray<FileJS> getFilesNative(Element e) /*-{
 		return e.dropzone.files;
     }-*/;
 
-    private native void initDropzone(Element e, DropzoneOptions options, DropzoneEventHandler handler,
-        DropzoneDictonary dictionary)
-    /*-{
+	private native void initDropzone(Element e, DropzoneOptions options, DropzoneEventHandler handler,
+			DropzoneDictonary dictionary)
+			/*-{
 		//if there is a dictionary, iterate over it and transfer the values
 
 		if (dictionary) {
@@ -262,69 +264,82 @@ public class Dropzone extends Composite {
 
     }-*/;
 
-    private void initWidget() {
-        // To conform with the standard stylesheet
-        final HTML widget = new HTML();
-        widget.setStylePrimaryName("dropzone");
-        initWidget(widget);
-    }
+	private void initWidget() {
+		// To conform with the standard stylesheet
+		final HTML widget = new HTML();
+		widget.setStylePrimaryName("dropzone");
+		initWidget(widget);
+	}
 
-    private void injectResources(Resources resources) {
-        ResourceInjector.configure(resources);
-    }
+	private void injectResources(Resources resources) {
+		ResourceInjector.configure(resources);
+	}
 
-    /**
-     *  Checks how many files are currently uploading,
-     *  and if it’s less than <code>options.parallelUploads</code>,
-     *  <code>.processFile(file)</code> is called.
-     *
-     *  <p>
-     *  If you set autoProcessQueue to <code>false</code>, then <code>.processQueue()</code> is
-     *  never called implicitly.
-     *  This means that you have to call it yourself when
-     *  you want to upload all files currently queued.
-     *  </p>
-     */
-    public void processQueue() {
-        processQueueNative(getElement());
-    }
+	/**
+	 *  Checks how many files are currently uploading,
+	 *  and if it’s less than <code>options.parallelUploads</code>,
+	 *  <code>.processFile(file)</code> is called.
+	 *
+	 *  <p>
+	 *  If you set autoProcessQueue to <code>false</code>, then <code>.processQueue()</code> is
+	 *  never called implicitly.
+	 *  This means that you have to call it yourself when
+	 *  you want to upload all files currently queued.
+	 *  </p>
+	 */
+	public void processQueue() {
+		processQueueNative(getElement());
+	}
 
-    private native void processQueueNative(Element e)/*-{
+	private native void processQueueNative(Element e)/*-{
 		e.dropzone.processQueue();
     }-*/;
 
-    /**
-     * Removes all files from dropzone.
-     */
-    public void removeAllFiles() {
-        removeAllFilesNative(getElement());
-    }
+	/**
+	 * Removes all files from dropzone.
+	 */
+	public void removeAllFiles() {
+		removeAllFilesNative(getElement());
+	}
 
-    /**
-     * Native implementation of {@link Dropzone#removeAllFiles()}
-     *
-     * @param e dropzone element
-     */
-    private native void removeAllFilesNative(Element e) /*-{
+	/**
+	 * Native implementation of {@link Dropzone#removeAllFiles()}
+	 *
+	 * @param e dropzone element
+	 */
+	private native void removeAllFilesNative(Element e) /*-{
 		e.dropzone.removeAllFiles(true);
     }-*/;
 
-    /**
-     * Removes file at index i.
-     *
-     * @param i index of the file to remove.
-     */
-    public void removeFile(int i) {
-        removeFileNative(getElement(), i);
-    }
+	/**
+	 * Removes file at index i.
+	 *
+	 * @param i index of the file to remove.
+	 */
+	public void removeFile(int i) {
+		removeFileNative(getElement(), i);
+	}
 
-    /**
-     * Native implementation of {@link Dropzone#removeFile(int)}
-     *
-     * @param e dropzone element
-     * @param i index of the file to remove
-     */
-    private native void removeFileNative(Element e, int i) /*-{
+	/**
+	 * Removes file at index i.
+	 *
+	 * @param i index of the file to remove.
+	 */
+	public void removeFile(File file) {
+		removeFileNative(getElement(), file);
+	}
+
+	private native void removeFileNative(Element e, File file)  /*-{
+		e.dropzone.removeFile(file);
+    }-*/;
+
+	/**
+	 * Native implementation of {@link Dropzone#removeFile(int)}
+	 *
+	 * @param e dropzone element
+	 * @param i index of the file to remove
+	 */
+	private native void removeFileNative(Element e, int i) /*-{
 		if (e.dropzone.files[i] != null) {
 			e.dropzone.removeFile(e.dropzone.files[0]);
 		}
--- src/test/java/com/github/andrezimmermann/dropzone/client/DropzoneTest.java
+++ src/test/java/com/github/andrezimmermann/dropzone/client/DropzoneTest.java
@@ -1,33 +1,89 @@
 package com.github.andrezimmermann.dropzone.client;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 import com.github.andrezimmermann.dropzone.client.interfaces.DropzoneOptions;
+import com.github.andrezimmermann.dropzone.client.interfaces.File;
 import com.google.gwt.junit.client.GWTTestCase;
 import com.google.gwt.user.client.ui.RootPanel;
 
 public class DropzoneTest extends GWTTestCase {
 
-    @Override
-    public String getModuleName() {
-        return "com.github.andrezimmermann.dropzone.TestModule";
-    }
+	@Override
+	public String getModuleName() {
+		return "com.github.andrezimmermann.dropzone.TestModule";
+	}
 
-    @Test
-    public void testShouldCreateDefaultDropzone() {
 
-        DropzoneOptions options = Dropzone.options();
-        options.setUrl("/upload");
+	@Test
+	public void testRemoveFileFromDropzone() {
+		//TODO Too long refactor to 4 separated test cases when in a proper IDE
+		Dropzone dropzone = createDefaultDropzoneWidget();
+		int filesExpected = 5;
 
-        Dropzone dropzone = new Dropzone(
-            options
-            );
+		for (int i = 0; i < filesExpected; i++) {
+			String indexString = String.valueOf(i);
+			dropzone.addFile("Fake File " + indexString, i, "/fakeFileUrl" + indexString);
+		}
 
-        assertNotNull(dropzone);
+		int filesCount = dropzone.getFilesCount();
 
-        RootPanel.get().add(dropzone);
-        assertTrue(dropzone.isAttached());
+		assertEquals("Files werent added properly"  ,filesExpected, filesCount);
 
-    }
+
+		ArrayList<File> files = dropzone.getFiles();
+
+		//Try delete the second 
+
+		File removedFile = files.get(1);
+		dropzone.removeFile(removedFile);
+
+		assertEquals(filesExpected -1, dropzone.getFilesCount());
+
+		files = dropzone.getFiles();
+
+		assertFalse("Should have removed the file!", files.contains(removedFile));
+
+		int indexToRemove = 0;
+		File anotherRemovedFile = files.get(indexToRemove);
+		dropzone.removeFile(indexToRemove);
+		
+		files = dropzone.getFiles();
+		
+		assertFalse("Should have removed the file!", files.contains(anotherRemovedFile));
+		
+		
+		dropzone.removeAllFiles();
+		
+		filesCount = dropzone.getFilesCount();
+		assertEquals("Files werent removed properly"  ,0, filesCount);
+
+	}
+
+	@Test
+	public void testShouldCreateDefaultDropzone() {
+
+		createDefaultDropzoneWidget();
+
+	}
+
+
+	private Dropzone createDefaultDropzoneWidget() {
+		DropzoneOptions options = Dropzone.options();
+		options.setUrl("/upload");
+
+		Dropzone dropzone = new Dropzone(
+				options
+				);
+
+		assertNotNull(dropzone);
+
+		RootPanel.get().add(dropzone);
+		assertTrue(dropzone.isAttached());
+
+		return dropzone;
+	}
 
 }
